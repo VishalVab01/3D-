38ac12e1c54b9a22eb5512b7a6c1598ab4ca0938
VishalVab <vishalbgp2006@gmail.com>
2025-12-21 20:48:37 +0530

    domain changed

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'http://localhost:3000/';
+        iframe.src = 'https://2dos-bice.vercel.app/';
         /**
          * Use dev server is query params are present
          *
@@ -194,7 +194,7 @@ export default class MonitorScreen extends EventEmitter {
          */
         const urlParams = new URLSearchParams(window.location.search);
         if (urlParams.has('dev')) {
-            iframe.src = 'http://localhost:3000/';
+            iframe.src = 'https://2dos-bice.vercel.app/';
         }
         iframe.style.width = this.screenSize.width + 'px';
         iframe.style.height = this.screenSize.height + 'px';
